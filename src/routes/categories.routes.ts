9fc29698159d95ac05c3f1ea5deaf5d3e6c93abe
taciano <tacianohora2020@hotmail.com>
2023-04-20 18:13:11 -0300

    feat: validar cadastro de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return res.status(400).json({ error: "Category Already Exists" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return res.status(201).send();
