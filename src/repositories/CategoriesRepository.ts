16512b2c0e152d560d088d614b6efa7979323f39
taciano <tacianohora2020@hotmail.com>
2023-04-21 21:51:02 -0300

    feat: aplicar solid

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,11 +1,9 @@
 import { Category } from "../models/Category";
+import { ICategoryRepository, ICreateCategoryDTO } from "./ICategoriesRepository";
 
-interface ICreateCategoryDTO {
-  name: string;
-  description: string;
-}
 
-class CategoriesRepository {
+
+class CategoriesRepository implements ICategoryRepository {
   private categories: Category[];
 
   constructor() {
